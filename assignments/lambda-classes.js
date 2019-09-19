836289b1c9ded386ca74e6bbd279bc3e89a4f0cf
MarquesC19 <Carter234>
2019-09-19 14:41:01 -0500

    working in lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,8 @@
 // CODE here for your Lambda Classes
+class person{
+    constructor(personAttrs) {
+        this.name = personAtts.name;
+        this.age = personAtts.age;
+        this.location = personAtts.location;
+        this.gender = personAtts.gender;
+    }
