99969a8e9001f5a246415c0d2575460988371e7f
MarquesC19 <Carter234>
2019-09-19 15:16:37 -0500

    adding some touch to lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,8 +1,35 @@
 // CODE here for your Lambda Classes
-class person{
+class Person{
     constructor(personAttrs) {
-        this.name = personAtts.name;
-        this.age = personAtts.age;
-        this.location = personAtts.location;
-        this.gender = personAtts.gender;
+        this.name = personAttrs.name;
+        this.age = personAttrs.age;
+        this.location = personAttrs.location;
+        this.gender = personAttrs.gender;
+    }
+    speak() {
+console.log( `Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+
+    class Intructor extends Person{
+        constructor(instructionAttrs){
+    super(instructorAttrs);
+    this.specialty = instructorAttrs.specialty;
+    this.favLanguage = instructorAttrs.favLanguage;
+    this.catchPhrase = instructorAttrs.catchPhrase;
+    }
+
+    class student extends Person{
+    constructor(studentAttrs) {
+    super(studentAttrs);
+    this.previousBackground=student.Attrs.perviousBackground;
+    this.className = studentAttrs.className;
+    this.favSubjects=StudentAttrs.FavSubjects;
+    this.grade = studentsAttrs.grade;
+    }
+    
+    class ProjectManager extends instructor{
+        constructor(managerAttrs) {
+    super(managerAttrs);
+    this.gradClassName = managerAttrs.gradClassName;
+    this.FavInstructor = managerAttrs.FavInstructor;
     }
